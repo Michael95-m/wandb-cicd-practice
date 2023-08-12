543ff4ad00afe665c47886d8aef814b73945739f
Min Khant Maung Maung <minkhantmgmg.mk19@gmail.com>
2023-08-12 06:41:44 +0630

    Update ci.py

--- ci.py
+++ ci.py
@@ -1,4 +1,4 @@
 import wandb
 
 print(f"Wandb version: {wandb.__version__}")
-assert wandb.__version__ == "0.15.0", f"Expected version didn't match and the current version is {wandb.__version__}. Please fix the version"
+assert wandb.__version__ == "0.15.8", f"Expected version didn't match and the current version is {wandb.__version__}. Please fix the version"
